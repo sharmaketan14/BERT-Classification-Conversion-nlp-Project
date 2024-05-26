f9d45e3a69b31144c651eb0847353f34fb31e5e5
ketansharma2021 <ketan.sharma2021@vitbhopal.ac.in>
2024-05-26 11:41:37 +0530

    Conversion Module Buggy

--- Classification.py
+++
@@ -1,8 +0,0 @@
-def classify_sentence(sentence, model):
-    sentence = np.array([[sentence]])
-    print(sentence)
-    #y_predict = model.predict(sentence)
-    y_predict = y_predict.flatten()
-    y_predict = np.where(y_predict > 0.5, 1, 0)
-    
-    return y_predict[0][0]
